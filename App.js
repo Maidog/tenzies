39196414ab72de7be228ed6e1f4b9b5007d6bca8
Maidog <65551346+Maidog@users.noreply.github.com>
2022-01-30 16:47:46 +0900

    initial commit

---
+++ App.js
@@ -0,0 +1,98 @@
+import React from "react"
+import Die from "./Die"
+import {nanoid} from "nanoid"
+
+export default function App() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    const [tenzies, setTenzies] = React.useState(false)
+    
+    
+    
+    React.useEffect(() => {
+        let heldCount = 0
+        for(let i = 0; i < dice.length; i++) {
+            const die = dice[i]
+            if (die.isHeld === true){
+                heldCount++
+            }
+        }
+        
+        let sameDice = false
+        for(let i = 0; i < 9; i++){
+            const die = dice[i]
+            const nextDie = dice[i+1]
+            if (die.value === nextDie.value) {
+                sameDice = true}
+            else{
+                sameDice = false
+            }
+        }
+        
+        if(heldCount === 10 && sameDice === true) {
+            setTenzies(true) 
+            console.log("you won!")
+        }
+    }, [dice])
+/**
+ * Challenge: Check the dice array for these winning conditions:
+ * 1. All dice are held, and
+ * 2. all dice have the same value
+ * 
+ * If both conditions are true, set `tenzies` to true and log
+ * "You won!" to the console
+ */
+
+    function generateNewDie() {
+        return {
+            value: Math.ceil(Math.random() * 6),
+            isHeld: false,
+            id: nanoid()
+        }
+    }
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(generateNewDie())
+        }
+        return newDice
+    }
+    
+    
+    function rollDice() {
+        setDice(oldDice => oldDice.map(die => {
+            return die.isHeld ? 
+                die :
+                generateNewDie()
+        }))
+    }
+    
+    function holdDice(id) {
+        setDice(oldDice => oldDice.map(die => {
+            return die.id === id ? 
+                {...die, isHeld: !die.isHeld} :
+                die
+        }))
+    }
+    
+    const diceElements = dice.map(die => (
+        <Die 
+            key={die.id} 
+            value={die.value} 
+            isHeld={die.isHeld} 
+            holdDice={() => holdDice(die.id)}
+        />
+    ))
+    
+    return (
+        <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+        </main>
+    )
+}
